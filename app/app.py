a5a329748292537cd200fb8627f0ee6c4a20868f
Mamdouh Khalaf <2210002016@iau.edu.sa>
2023-09-06 09:49:53 +0300

    implemented super simple post put delete methods

--- app/app.py
+++ app/app.py
@@ -7,18 +7,40 @@ async def root() -> dict:
     return {'message' : 'Hello World'}
 
 
-@app.get('/todo', tags=['get list of todos'])
+@app.get('/todo', tags=['todos'])
 async def get_todo() -> dict:
-    return {'todos', todos}
+    return {'data': todos}
 
+@app.post('/todo', tags = ['todos'])
+async def add_todo(todo:dict) -> dict:
+    todos.append(todo)
+    return {'data' : 'todo added successfully'}
+
+@app.put('/todo/{id}', tags= ['todos'])
+async def update_todo(id: int, body: dict) -> dict:
+    for todo in todos:
+        if int((todo['id'])) == id:
+            todo['Activity'] = body['Activity']
+            return {'data' : f'todo with {id} has been updated successfully'}
+        
+    return {'data' : f'todo with {id} not found'}
+         
+@app.delete('/todo/{id}', tags= ['todos'])
+async def delete_todo (id: int) -> dict:
+    for todo in todos:
+        if int((todo['id'])) == id:
+            todos.remove(todo)
+            return {'data' : f'todo with {id} has been deleted successfully'}
+        
+    return {'data' : f'todo with {id} not found'}
 
 todos = [
     {
      "id" : 1,
      "Activity" : "Go to class"   
-    },
+    }, 
     {
      "id" : 2,
      "Activity" : "Meet with Ali"   
     }
-]
+]  
