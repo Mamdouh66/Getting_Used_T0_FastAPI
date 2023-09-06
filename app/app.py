411d9a35c96aa301e8d9982d225a95f9cf40410d
Mamdouh Khalaf <2210002016@iau.edu.sa>
2023-09-06 09:12:24 +0300

    implemented get

--- app/app.py
+++ app/app.py
@@ -5,3 +5,20 @@ app = FastAPI()
 @app.get('/', tags= ['Root'])
 async def root() -> dict:
     return {'message' : 'Hello World'}
+
+
+@app.get('/todo', tags=['get list of todos'])
+async def get_todo() -> dict:
+    return {'todos', todos}
+
+
+todos = [
+    {
+     "id" : 1,
+     "Activity" : "Go to class"   
+    },
+    {
+     "id" : 2,
+     "Activity" : "Meet with Ali"   
+    }
+]
