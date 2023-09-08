c1da819d7797886d55554b8fc98b3b2cf1774c17
Mamdouh Khalaf <2210002016@iau.edu.sa>
2023-09-08 14:36:10 +0300

    dabbled with the response model

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from typing import List
 
 app = FastAPI()
     
@@ -25,8 +26,10 @@ async def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_blog(id,  db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
+
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'blog with id ({id}) was not found')
+    
     blog.delete(synchronize_session = False)
     db.commit()
     return {'details' : 'Blog was deleted successfully!!!'}
@@ -34,21 +37,25 @@ async def delete_blog(id,  db: Session = Depends(get_db)):
 @app.put('/blog/{id}', status_code = status.HTTP_202_ACCEPTED)
 async def update_blog(id, request: schemas.Blog,  db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
+
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'blog with id ({id}) was not found')
+    
     blog.update(request)
     db.commit()
     return {'details' : 'updated succesfully'}
 
-@app.get('/blog')
+@app.get('/blog', response_model = List[schemas.GetBlog])
 async def get_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}', status_code = status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code = status.HTTP_200_OK, response_model = schemas.GetBlog)
 async def get_blog(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f'Blog with {id} was not found')
+    
     return blog
