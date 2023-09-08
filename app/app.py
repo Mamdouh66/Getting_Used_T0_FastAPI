c172d52af6d4820016011831e2922aaac44634bf
Mamdouh Khalaf <2210002016@iau.edu.sa>
2023-09-08 14:24:52 +0300

    implemented update and delete with db

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+
 app = FastAPI()
     
 models.Base.metadata.create_all(engine)
@@ -13,7 +14,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+@app.post('/blog', status_code = status.HTTP_201_CREATED)
 async def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title = request.title, body = request.body)
     db.add(new_blog)
@@ -21,12 +22,33 @@ async def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_blog(id,  db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'blog with id ({id}) was not found')
+    blog.delete(synchronize_session = False)
+    db.commit()
+    return {'details' : 'Blog was deleted successfully!!!'}
+
+@app.put('/blog/{id}', status_code = status.HTTP_202_ACCEPTED)
+async def update_blog(id, request: schemas.Blog,  db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'blog with id ({id}) was not found')
+    blog.update(request)
+    db.commit()
+    return {'details' : 'updated succesfully'}
+
 @app.get('/blog')
 async def get_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
-async def get_blog(id, db: Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code = status.HTTP_200_OK)
+async def get_blog(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f'Blog with {id} was not found')
     return blog
